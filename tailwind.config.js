08a3fbf4c7b5daf98c57b0daaebee6998164bcdd
Ezira Ashenafi <eazash22@gmail.com>
2024-05-28 21:16:08 +0300

    feat: add tailwindcss module
    - Add tailwindcss nuxt module
    - Configure Editor for tailwind directives
    - Extend theme to add brand colors and override blacks & whites

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        // Color Overrides
+        'black': '#0F0F20',
+        'white': '#F0F0F0',
+        // Brand Colors
+        'midnight-blue': '#0B0B1A',
+        'navy-blue': '#000080',
+        'rich-indigo': '#2F195F',
+        'vibrant-purple': '#7353BA',
+      },
+    },
+  },
+  plugins: [],
+}
