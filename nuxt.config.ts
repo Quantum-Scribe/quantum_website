dfe4ca4f371ca5d443f407f1bab36a1d00b8efb1
Ezira Ashenafi <eazash22@gmail.com>
2024-06-03 14:37:05 +0300

    feat: add nuxt image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/fonts", "nuxt-icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/fonts",
+    "nuxt-icon",
+    "@nuxt/image"
+  ],
   eslint: {
     config: { stylistic: false, standalone: false },
   },
