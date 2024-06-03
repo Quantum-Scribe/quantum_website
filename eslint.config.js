52987e2371256046a22d238f01a627652040a075
Ezira Ashenafi <eazash22@gmail.com>
2024-06-03 14:37:23 +0300

    chore: update linting rule for component naming (use kebab-case)

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,14 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt(antfu({
   formatters: true,
   vue: true,
+}, {
+  files: ['components/**/*.vue', 'layouts/**/*.vue'],
+  rules: {
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+      },
+    ],
+  },
 }))
