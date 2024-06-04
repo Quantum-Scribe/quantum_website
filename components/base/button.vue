eec9f84997dd839383cd297b0f0b169502991ba1
Ezira Ashenafi <eazash22@gmail.com>
2024-06-04 09:19:29 +0300

    feat: add base button component

---
+++ components/base/button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { cva } from 'class-variance-authority'
+import { twMerge } from 'tailwind-merge'
+
+const variants = cva('py-3 px-5 md:px-10 rounded-md', {
+  variants: {
+    variant: {
+      filled: 'bg-white text-black',
+      outlined: 'bg-transparent border-2 text-white border-white',
+    }
+  },
+})
+
+type ButtonProps = {
+  variant: NonNullable<Parameters<typeof variants>[0]>['variant']
+}
+
+const props = defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button :class="twMerge(variants(props))" >
+    <slot />
+  </button>
+</template>
